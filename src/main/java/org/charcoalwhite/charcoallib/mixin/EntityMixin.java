4f708f9baf0ee41ecf3ea68dbdf98f727b0819a9
CharcoalWhite <125453632+CharcoalWhite@users.noreply.github.com>
2024-08-24 14:55:18 +0800

    0.1.9

--- src/main/java/org/charcoalwhite/charcoallib/mixin/EntityMixin.java
+++ src/main/java/org/charcoalwhite/charcoallib/mixin/EntityMixin.java
@@ -3,12 +3,12 @@ package org.charcoalwhite.charcoallib.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.s2c.play.EntityPassengersSetS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
-
 import org.charcoalwhite.charcoallib.api.EntityApi;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin implements EntityApi{
@@ -25,7 +25,7 @@ public abstract class EntityMixin implements EntityApi{
             shift = At.Shift.AFTER
         )
     )
-    private void sendPacketAddPassenger(Entity passenger) {
+    private void sendPacketAddPassenger(Entity passenger, CallbackInfo ci) {
         if (this.isPlayer()) {
             ((ServerPlayerEntity)(Object)this).networkHandler.sendPacket(new EntityPassengersSetS2CPacket(((Entity)(Object)this)));
         }
@@ -39,7 +39,7 @@ public abstract class EntityMixin implements EntityApi{
             shift = At.Shift.AFTER
         )
     )
-    private void sendPacketRemovePassenger(Entity passenger) {
+    private void sendPacketRemovePassenger(Entity passenger, CallbackInfo ci) {
         if (this.isPlayer()) {
             ((ServerPlayerEntity)(Object)this).networkHandler.sendPacket(new EntityPassengersSetS2CPacket(((Entity)(Object)this)));
         }
